592c3ef8eda27ff4cd8b29b30404d56400e0dd76
Tom Marler <thomas.marler@hotmail.com>
2019-09-15 09:32:53 -0600

    Add user and role domain classes

---
+++ src/main/java/com/tommarler/growthDragon/GrowthDragonApplication.java
@@ -0,0 +1,14 @@
+package com.tommarler.growthDragon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowthDragonApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GrowthDragonApplication.class, args);
+	}
+
+}
