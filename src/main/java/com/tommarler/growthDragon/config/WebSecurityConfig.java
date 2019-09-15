2de8b3c483a9a8f5e469af24268ff984d22ee2e9
Tom Marler <thomas.marler@hotmail.com>
2019-09-15 14:30:11 -0600

    Added login, home, newFeed and default pages

--- src/main/java/com/tommarler/growthDragon/config/WebSecurityConfig.java
+++ src/main/java/com/tommarler/growthDragon/config/WebSecurityConfig.java
@@ -58,7 +58,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web
                 .ignoring()
-                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+                .antMatchers("/resources/**", "/static/**", "/static/css/css**", "/static/js/js/**", "/images/**");
     }
 
 }
